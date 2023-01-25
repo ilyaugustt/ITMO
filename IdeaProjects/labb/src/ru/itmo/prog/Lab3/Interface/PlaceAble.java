0303ac56afc465a990f687861ee5f19b05cf1c3d
ilyaugustt <118938328+ilyaugustt@users.noreply.github.com>
2023-01-25 06:19:49 +0300

    Delete IdeaProjects/labb/src/ru/itmo/prog/Lab3 directory

--- IdeaProjects/labb/src/ru/itmo/prog/Lab3/Interface/PlaceAble.java
+++
@@ -1,9 +0,0 @@
-package ru.itmo.prog.Lab3.Interface;
-
-import ru.itmo.prog.Lab3.Place.Places;
-
-public interface PlaceAble {
-    String getPlace();
-    void setType(Places type);
-    Places getType();
-}
--- IdeaProjects/labb/src/ru/itmo/prog/Lab3/Place/Places.java
+++
@@ -1,6 +0,0 @@
-package ru.itmo.prog.Lab3.Place;
-
-public enum Places {
-    Forest,
-    Moon;
-}
